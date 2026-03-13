5ab9f0086096ae177c81f941b22cb015d8f0c74e
Liam Scrase <liamscrase1@gmail.com>
2026-03-13 19:27:33 +1300

    Refactor navigation and footer links; update styles and add responsive behavior
    - Updated navigation styles in CSS and SCSS for better responsiveness.
    - Changed footer links from "Parish Life" to "Parish News" for consistency.
    - Modified header dropdown and offcanvas menu to reflect the new "Parish News" terminology.
    - Enhanced layout metadata with descriptions for SEO optimization across various pages.
    - Removed the "Parish Life" page and created a new "Parish News" page to consolidate content.
    - Updated TypeScript configuration to include Node types for better compatibility.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://stnicholas.github.io',
+  site: 'https://www.stnicholaschurch.org.nz',
   base: '/',
+  integrations: [sitemap()],
 });
