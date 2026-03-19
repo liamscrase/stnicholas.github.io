783e2589732772006b1ab947a67ad2d1fbe3b31e
Liam Scrase <Liam.Scrase@avantifinance.co.nz>
2026-03-20 09:54:38 +1300

    refactor: add redirects for improved navigation

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,14 @@ export default defineConfig({
   output: 'static',
   site: 'https://www.stnicholaschurch.org.nz',
   integrations: [sitemap()],
+  redirects: {
+    '/gallery': '/icon-gallery',
+    '/gallery/': '/icon-gallery',
+    '/contact-us': '/contact',
+    '/contact-us/': '/contact',
+    '/history': '/',
+    '/history/': '/',
+    '/sample-page': '/',
+    '/sample-page/': '/',
+  },
 });
